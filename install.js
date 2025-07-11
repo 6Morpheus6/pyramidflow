3a8544a4bef887608181bad66d28d5502f6eb386
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-07-11 11:35:28 +0200

    pin gradio
    fixes blank screen

--- install.js
+++ install.js
@@ -56,7 +56,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install gradio",
+          "uv pip install gradio==5.35.0",
           "uv pip install -r requirements.txt"
         ]
       }
